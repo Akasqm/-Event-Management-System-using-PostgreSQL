89b40688fa2da021899ce0388827fe293ab28933
Akasqm <119439982+Akasqm@users.noreply.github.com>
2024-08-28 11:15:41 +0530

    Add files via upload

---
+++ 3. Manage Event Details/3)a. Manage Event Details - Inserting a new event..sql
@@ -0,0 +1,3 @@
+INSERT INTO Events (Event_Name, Event_Date, Event_Location, Event_Description)
+VALUES ('Magic world 2024', '2024-11-10', 'Banglore, Karnataka', 'Annual Magic exposition');
+
---
+++ 3. Manage Event Details/3)b. Updating an event's information..sql
@@ -0,0 +1,4 @@
+UPDATE Events
+SET Event_Name = 'Music Festival', Event_Date = '2024-08-25', Event_Location = 'Kochi, Kerala'
+WHERE Event_Id = 3;
+
